7696c2d0c6db921d9bb710d6fc9ce33fb60d8d58
Nico Proto <nicolasproto@gmail.com>
2020-11-27 11:27:29 +0100

    plants with tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,46 @@
 
 Garden.destroy_all if Rails.env.development?
 
-Garden.create!(
-  name: 'My little garden',
-  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
-)
 
 Garden.create!(
   name: 'My other garden',
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
 )
+
+Garden.destroy_all if Rails.env.development?
+
+little = Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+other = Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+Plant.create!(
+  name: "Monstera",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg",
+  garden: little
+)
+
+Plant.create!(
+  name: "Philo",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg",
+  garden: little
+)
+
+Plant.create!(
+  name: "Dieff",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg",
+  garden: other
+)
+
+names = ["Fruit", "Cactus", "Roses", "Tulips", "Orchids", "Christmas tree"]
+
+names.each do |tag_name|
+  tag = Tag.new(name: tag_name)
+  tag.save!
+  puts "#{tag.name} created!"
+end
